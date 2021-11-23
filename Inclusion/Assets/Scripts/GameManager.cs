ea29a0290d5a80659dd746c0eb3f1139ab9e4688
Lior Tal <ljtal7@gmail.com>
2021-11-24 00:58:51 +0200

    folder org and game manager

---
+++ Inclusion/Assets/Scripts/GameManager.cs
@@ -0,0 +1,139 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI countdownText;
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private TextMeshProUGUI livesText;
+    private bool timerGoing;
+    private float score = 0f;
+    private float pointsPerSecond = 10;
+    [SerializeField] private int playerLives = 3;
+
+    private void Awake()
+    {
+        timerGoing = false;
+        scoreText.text = "0";
+        livesText.text = "3";
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+        StartCoroutine(CountdownToStart());
+    }
+
+    IEnumerator CountdownToStart()
+    {
+        int countdownTime = 3;
+        countdownText.text = countdownTime.ToString();
+        countdownText.gameObject.SetActive(true);
+        yield return new WaitForSeconds(0.25f);
+
+        while (countdownTime > 0)
+        {
+            countdownText.text = countdownTime.ToString();
+            yield return new WaitForSeconds(1f);
+            countdownTime--;
+        }
+
+        countdownText.text = "GO!";
+        yield return new WaitForSeconds(1f);
+        countdownText.gameObject.SetActive(false);
+        BeginGame();
+    }
+
+    private void BeginGame()
+    {
+        score = 0f;
+        playerLives = 3;
+        StartTimer();
+    }
+
+    IEnumerator UpdateTimer()
+    {
+        while (timerGoing)
+        {
+            score += pointsPerSecond * Time.deltaTime;
+            scoreText.text = Mathf.FloorToInt(score).ToString();
+            yield return null;
+        }
+    }
+
+    private void StartTimer()
+    {
+        timerGoing = true;
+        StartCoroutine(UpdateTimer());
+    }
+
+    private void StopTimer()
+    {
+        timerGoing = false;
+    }
+
+    private void UpdateScore()
+    {
+        score += 50;
+    }
+
+    private void UpdateStrikes()
+    {
+        playerLives--;
+
+        if(playerLives <= 0)
+        {
+            LoseGame();
+        }
+    }
+
+    private void LoseGame()
+    {
+        Time.timeScale = 0f;
+        StopTimer();
+
+        //if (PlayerPrefs.GetInt("highscore", 0) < Mathf.FloorToInt(score))
+        //{
+        //    texts[2].text = "New High Score!";
+        //    texts[3].text = texts[1].text;
+        //    PlayerPrefs.SetInt("highscore", Mathf.FloorToInt(score));
+        //}
+
+        //else
+        //{
+        //    texts[2].text = "Final Score: " + texts[1].text;
+        //    texts[3].text = "High Score: " + PlayerPrefs.GetInt("highscore", 0).ToString();
+        //}
+
+        //panels[1].SetActive(true);
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+        StopTimer();
+        //panels[0].SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        StartTimer();
+        //panels[0].SetActive(false);
+        //gameplayObjects[0].SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+        //panels[0].SetActive(false);
+        //panels[1].SetActive(false);
+        playerLives = 3;
+        scoreText.text = "0";
+        livesText.text = "3";
+        Time.timeScale = 1f;
+        StartCoroutine(CountdownToStart());
+    }
+}
