46761ee33290cc914f4a81881d0fe8da57c247c8
Lior Tal <ljtal7@gmail.com>
2021-11-24 17:48:55 +0200

    code cleanup and extensibility

--- Inclusion/Assets/Scripts/PlayerColor.cs
+++ Inclusion/Assets/Scripts/PlayerColor.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class PlayerColor : Character
 {
-    [SerializeField] private Renderer child1Rend;
-    [SerializeField] private Renderer child2Rend;
+    [SerializeField] private Renderer[] childRenderers;
     [SerializeField] private List<int> childColors;
 
     private void Awake()
     {
-        childColors = new List<int> { 9, 9 };
+        childColors = new List<int> { 9, 9, 9 };
     }
 
     private void OnEnable()
@@ -46,22 +45,8 @@ public class PlayerColor : Character
 
         GameObject spawnedVFX = Instantiate(particleEffects[3], transform.position + particleOffset, Quaternion.identity);
         Destroy(spawnedVFX, 1);
-
-        switch (i)
-        {
-            case 0:
-                rend.material = yellow;
-                activeMaterial = 0;
-                break;
-            case 1:
-                rend.material = red;
-                activeMaterial = 1;
-                break;
-            case 2:
-                rend.material = green;
-                activeMaterial = 2;
-                break;
-        }
+        rend.material = materials[i];
+        activeMaterial = i;
     }
 
     public int GetActiveMaterial()
@@ -71,24 +56,17 @@ public class PlayerColor : Character
 
     private void AddToLine(int playerMaterial)
     {
+        Debug.Log(transform.childCount.ToString());
         if (childColors.Contains(playerMaterial))
         {
             return;
         }
 
-        for (int i = 1; i < 3; i++)
+        for (int i = 1; i < transform.childCount; i++)
         {
             if (!transform.GetChild(i).gameObject.activeInHierarchy)
             {
-                switch (i)
-                {
-                    case 1:
-                        child1Rend.material = materials[playerMaterial];
-                        break;
-                    case 2:
-                        child2Rend.material = materials[playerMaterial];
-                        break;
-                }
+                childRenderers[i-1].material = materials[playerMaterial];
                 transform.GetChild(i).gameObject.SetActive(true);
                 childColors[i-1] = playerMaterial;
                 return;
@@ -98,14 +76,26 @@ public class PlayerColor : Character
 
     //private void RemoveFromLine()
     //{
-    //    List<int> childColors = new List<int>();
-
     //    for (int i = 1; i < 3; i++)
     //    {
     //        if (transform.GetChild(i).gameObject.activeInHierarchy)
     //        {
-    //            childColors.Add(transform.GetChild(0))
-    //            transform.GetChild(i).gameObject.SetActive(false);
+    //            for(int j = i; j < 3; j++)
+    //            {
+    //                switch (j)
+    //                {
+    //                    case 1:
+    //                        child1Rend.material = materials[playerMaterial];
+    //                        break;
+    //                    case 2:
+    //                        child2Rend.material = materials[playerMaterial];
+    //                        break;
+    //                    case 3:
+    //                        child3Rend.material = materials[playerMaterial];
+    //                        break;
+    //                }
+    //                transform.GetChild(j).gameObject.SetActive(false);
+    //            }
     //            return;
     //        }
     //    }
