c0826098e0a7c590ed37c9ae6f34cde9c2ebeec4
Lior Tal <ljtal7@gmail.com>
2021-11-29 00:28:41 +0200

    player colors and dancing

--- Inclusion/Assets/Scripts/HelperClass.cs
+++ Inclusion/Assets/Scripts/HelperClass.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public static class HelperClass
 {
-    public static Color playerRed = new Color(0.45f, 0.15f, 0.15f, 1);
+    public static Color playerOrange = new Color(0.5f, 0.2f, 0f, 1);
     public static Color playerGreen = new Color(0.1f, 0.3f, 0.1f, 1);
-    public static Color playerBlue = new Color(0.15f, 0.15f, 0.45f, 1);
-    public static List<Color> playerColors = new List<Color> { playerRed, playerGreen, playerBlue };
+    public static Color playerPurple = new Color(0.3f, 0f, 0.4f, 1);
+    public static List<Color> playerColors = new List<Color> { playerOrange, playerGreen, playerPurple };
 
-    public static Color particleRed = new Color(1f, 0.15f, 0.15f, 1);
+    public static Color particleOrange = new Color(0.5f, 0.2f, 0f, 1);
     public static Color particleGreen = new Color(0.15f, 1f, 0.15f, 1);
-    public static Color particleBlue = new Color(0.15f, 0.15f, 1f, 1);
-    public static List<Color> particleColors = new List<Color> { particleRed, particleGreen, particleBlue };
+    public static Color particlePurple = new Color(0.3f, 0f, 0.4f, 1);
+    public static List<Color> particleColors = new List<Color> { particleOrange, particleGreen, particlePurple };
 }
