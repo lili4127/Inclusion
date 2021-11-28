c1ad6b3d0dca9a6380337714b90191d792cf7c08
Lior Tal <ljtal7@gmail.com>
2021-11-28 19:39:13 +0200

    enemies turned around

--- Inclusion/Assets/Scripts/HelperClass.cs
+++ Inclusion/Assets/Scripts/HelperClass.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public static class HelperClass
 {
-    public static Color playerRed = new Color(0.35f, 0.15f, 0.15f, 1);
-    public static Color playerGreen = new Color(0.1f, 0.2f, 0.1f, 1);
-    public static Color playerBlue = new Color(0.15f, 0.15f, 0.35f, 1);
+    public static Color playerRed = new Color(0.45f, 0.15f, 0.15f, 1);
+    public static Color playerGreen = new Color(0.1f, 0.3f, 0.1f, 1);
+    public static Color playerBlue = new Color(0.15f, 0.15f, 0.45f, 1);
     public static List<Color> playerColors = new List<Color> { playerRed, playerGreen, playerBlue };
 
     public static Color particleRed = new Color(1f, 0.15f, 0.15f, 1);
