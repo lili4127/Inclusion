a009e970f29f4fc203ec69c3ed37d29069724fec
Lior Tal <ljtal7@gmail.com>
2021-12-07 22:56:00 +0200

    particle colors

--- Inclusion/Assets/Scripts/HelperClass.cs
+++ Inclusion/Assets/Scripts/HelperClass.cs
@@ -9,8 +9,8 @@ public static class HelperClass
     public static Color playerPurple = new Color(0.3f, 0f, 0.4f, 1);
     public static List<Color> playerColors = new List<Color> { playerOrange, playerGreen, playerPurple };
 
-    public static Color particleOrange = new Color(0.5f, 0.2f, 0f, 1);
-    public static Color particleGreen = new Color(0.15f, 1f, 0.15f, 1);
-    public static Color particlePurple = new Color(0.3f, 0f, 0.4f, 1);
+    public static Color particleOrange = new Color(1f, 0.647f, 0f, 1);
+    public static Color particleGreen = new Color(0f, 1f, 0f, 1);
+    public static Color particlePurple = new Color(0.749f, 0.251f, 0.749f, 1);
     public static List<Color> particleColors = new List<Color> { particleOrange, particleGreen, particlePurple };
 }
